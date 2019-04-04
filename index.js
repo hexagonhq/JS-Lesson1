185a1f469b8d58df0fef15a546da10c7ca16d238
robert <gnom784@mail.ru>
2019-04-04 14:27:33 +0300

    lesson 1

---
+++ index.js
@@ -0,0 +1,30 @@
+// task 1
+var seconds = 3 * (60 * 60);
+console.log('result:', seconds);
+
+// task 2
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num += 1;
+num -= 1;
+
+alert(num);
+
+// task 3
+var a = 10, b = 2, c = 5;
+var sum = a + b + c;
+alert(sum);
+
+// task 4
+var a = 17, b = 10;
+c = a - b;
+var d = 7;
+var result = c + d;
+alert(result);
+
+// task 5
+var name = 'Dmitry';
+alert('Привет ' + name + '!');
